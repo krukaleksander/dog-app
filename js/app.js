c3892d624aa0605a1c36cd9a9fbf35efff5a9a3f
Alek Kruk <kruk.aleksander@hotmail.com>
2020-08-10 16:57:28 +0200

    fns move to class

--- js/app.js
+++ js/app.js
@@ -1,21 +1,37 @@
-const listAllBreeds = () => {
-    return fetch('https://dog.ceo/api/breeds/list/all')
-        .then(resp => resp.json())
-        .then(data => data.message);
+class Doggo {
+    constructor() {
+        this.apiUrl = 'https://dog.ceo/api';
+        this.imgElement = document.querySelector('.featured-dog img');
+        this.init();
+    }
 
-};
+    listAllBreeds() {
+        return fetch(`${this.apiUrl}/breeds/list/all`)
+            .then(resp => resp.json())
+            .then(data => data.message);
 
-function getRandomImage() {
-    return fetch('https://dog.ceo/api/breeds/image/random')
-        .then(resp => resp.json())
-        .then(data => data.message)
-};
+    };
 
-function getRandomImageByBreed(breed) {
-    return fetch(`https://dog.ceo/api/breed/${breed}/images/random`)
-        .then(resp => resp.json())
-        .then(data => data.message);
-};
+    getRandomImage() {
+        return fetch(`${this.apiUrl}/breeds/image/random`)
+            .then(resp => resp.json())
+            .then(data => data.message)
+    };
 
-getRandomImageByBreed('boxer')
-    .then(src => console.log(src))
+    getRandomImageByBreed(breed) {
+        return fetch(`${this.apiUrl}/breed/${breed}/images/random`)
+            .then(resp => resp.json())
+            .then(data => data.message);
+    };
+    init() {
+        this.getRandomImage()
+            .then(src => this.imgElement.setAttribute('src', src));
+        this.listAllBreeds()
+            .then(breeds => console.log(breeds));
+    }
+
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    new Doggo();
+});
